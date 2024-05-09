9c253df8aad956b0af6885d35e9ddd665b4460a8
junseublim <junslim11@gmail.com>
2024-05-10 01:10:47 +0900

    feat: Implement upload page

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var cachedPrima: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrima) {
+    global.cachedPrima = new PrismaClient();
+  }
+  prisma = global.cachedPrima;
+}
+
+export const db = prisma;
