ed462afb9ad789273568e1da372327a9372e542a
junseublim <junslim11@gmail.com>
2024-05-11 16:49:27 +0900

    feat: Implement Design Configurator Page

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,8 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { z } from "zod";
+import sharp from "sharp";
+import { db } from "@/db";
 
 const f = createUploadthing();
 
@@ -14,9 +16,35 @@ export const ourFileRouter = {
     .middleware(async ({ input }) => {
       return { input };
     })
-    .onUploadComplete(async ({ metadata }) => {
+    .onUploadComplete(async ({ metadata, file }) => {
       const { configId } = metadata.input;
-      return { configId };
+
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
+
+      const imgMetadata = await sharp(buffer).metadata();
+      const { width, height } = imgMetadata;
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            width: width || 500,
+            height: height || 500,
+          },
+        });
+
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: { id: configId },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+
+        return { configId };
+      }
     }),
 } satisfies FileRouter;
 
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,7 +1,37 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
 import React from "react";
+import DesignConfigurator from "./DesignConfigurator";
 
-const page = async () => {
-  return <div>page</div>;
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { width, height, imageUrl } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageDimension={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
 };
 
 export default page;
